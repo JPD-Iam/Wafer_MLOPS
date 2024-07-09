0bfa83b207dc52df533b173a6391d69aaf8f1083
jyoti prakash dey <jyotiprakashdey@JPD.lan>
2024-07-09 15:16:20 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='application to classify Wafers using MLOPS',
+    author='Iam-JPD',
+    license='MIT',
+)
